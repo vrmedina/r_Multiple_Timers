007b432d6c9a93eacf4d5259330a8d5c77c225aa
Victor <vrmm1998@gmail.com>
2024-02-18 23:21:54 -0500

    add timer component styles

--- src/timers/components/Timer.jsx
+++ src/timers/components/Timer.jsx
@@ -1,21 +1,23 @@
 export const Timer = () => {
   return (
     <>
-      <div className='relative flex w-full max-w-[12rem] flex-col rounded-xl bg-gradient-to-tr from-gray-600 to-gray-400 bg-clip-border p-8 text-white shadow-md shadow-pink-500/40 m-3'>
-        <button className="flex justify-center rounded-lg bg-red-500 hover:bg-red-700 text-lg font-bold w-2.5">
-            X
-        </button>
-        <div className='relative m-0 mb-8 overflow-hidden rounded-none border-b border-white/10 bg-transparent bg-clip-border pb-8 text-center text-gray-700 shadow-none'>
-          <p className='block font-sans text-sm font-normal uppercase leading-normal text-white antialiased'>
-            Mi Temporizador
-          </p>
-          <h1 className='mt-2 flex justify-center gap-0 font-sans text-2xl font-normal tracking-normal text-white antialiased'>
-            1h 30m 25s
+      <div className='relative flex w-full max-w-[14rem] flex-col rounded-xl bg-gradient-to-tr from-gray-900 to-gray-500 bg-clip-border p-2 text-slate-100 shadow-md shadow-slate-500/80 m-1'>
+        <div className='flex-col relative m-0 my-2 overflow-hidden rounded-no bg-transparent bg-clip-border text-center text-gray-700 shadow-none'>
+          <div className="flex align-middle text-center justify-between">
+            <p className='flex self-start font-sans text-sm font-normal uppercase leading-normal text-white antialiased py-2.5 ms-3'>
+              Mi Temporizador
+            </p>
+            <button className='flex self-end w-10 select-none rounded-lg bg-red-500 py-2.5 me-2 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-black/30 justify-center hover:bg-red-400 hover:text-white active:bg-red-600 active:text-slate-100'>
+              X
+            </button>
+          </div>
+          <h1 className='flex-3 my-3 block justify-center gap-0 font-sans text-4xl tracking-normal text-white antialiased'>
+            15 : 24 : 36
           </h1>
         </div>
-        <div className='m-0 p-0'>
+        <div className='m-0 mx-2 mb-2'>
           <button
-            className='flex w-full select-none rounded-lg bg-green-500 py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-blue-gray-500/10 transition-all hover:scale-[1.02] hover:shadow-lg hover:shadow-blue-gray-500/20 focus:scale-[1.02] focus:opacity-[0.85] focus:shadow-none active:scale-100 active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none justify-center'
+            className='flex w-full select-none rounded-lg bg-green-500 py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-slate-100 shadow-md shadow-black/30 justify-center hover:bg-green-400 hover:text-white active:bg-green-600 active:text-slate-100'
             type='button'
             data-ripple-dark='true'
           >
--- src/timers/pages/Timers.jsx
+++ src/timers/pages/Timers.jsx
@@ -2,9 +2,16 @@ import { Timer } from '../components/Timer';
 
 export const Timers = () => {
   return (
-    <div className='inline-flex justify-center container mx-auto'>
+    <div className='flex flex-wrap justify-center container mx-auto'>
       <Timer />
       <Timer />
+      <Timer />
+      <Timer />
+      <Timer />
+      <Timer />
+      <Timer />
+      <Timer />
+      {/* <Timer /> */}
     </div>
   );
 };
